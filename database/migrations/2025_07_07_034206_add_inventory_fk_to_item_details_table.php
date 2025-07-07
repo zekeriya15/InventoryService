9a57dc1678af4717e16377923c42099e75147658
Muhamad Dzikriansyah <dzikriansyah1503@gmail.com>
2025-07-07 11:03:41 +0700

    add relation in item_details to loan_requests, inventories

---
+++ database/migrations/2025_07_07_034206_add_inventory_fk_to_item_details_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('item_details', function (Blueprint $table) {
+            $table->foreign('inventory_id')->references('id')->on('inventories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('item_details', function (Blueprint $table) {
+            $table->dropForeign(['inventory_id']);
+        }); 
+    }
+};
